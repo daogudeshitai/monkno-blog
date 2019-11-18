aaf2966054ca239f9f6c7332a57f467b9403fdce
daogudeshitai <15873863579@163.com>
2019-11-18 16:03:18 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Daogu's Blog`,
+    author: `monknoDaogu`,
+    description: `About me.`,
+    siteUrl: `https://daogu.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `daogudeshitai`,
     },
   },
   plugins: [
